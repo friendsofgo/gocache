041ce959786c210d4d869644eb125bc235658290
Joan López de la Franca Beltran <joanjan14@gmail.com>
2020-04-15 02:14:25 +0200

    Initial commit

---
+++ cache.go
@@ -0,0 +1,18 @@
+package gocache
+
+type Cache struct {
+	items map[string]interface{}
+}
+
+func (c *Cache) Get(key string) interface{} {
+	item, ok := c.items[key]
+	if !ok {
+		return nil
+	}
+
+	return item
+}
+
+func (c *Cache) Set(key string, value interface{}) {
+	c.items[key] = value
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/friendsofgo/gocache
+
+go 1.13
