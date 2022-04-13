851f0fcb3b546fc44927e89cff1627055f77b861
Joan López de la Franca Beltran <joanjan14@gmail.com>
2022-04-13 23:50:14 +0200

    Add support for concurrency

--- cache.go
+++ cache.go
@@ -1,6 +1,9 @@
 package gocache
 
+import "sync"
+
 type Cache[V any] struct {
+	sync.RWMutex
 	items map[string]V
 }
 
@@ -11,6 +14,9 @@ func New[V any]() *Cache[V] {
 }
 
 func (c *Cache[V]) Get(key string) V {
+	c.RLock()
+	defer c.RUnlock()
+
 	var noop V
 
 	item, ok := c.items[key]
@@ -22,5 +28,8 @@ func (c *Cache[V]) Get(key string) V {
 }
 
 func (c *Cache[V]) Set(key string, value V) {
+	c.Lock()
+	defer c.Unlock()
+
 	c.items[key] = value
 }
